8df6fea74c1f80b2da91cbbb44de4a7b663e9eb5
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-03-06 15:29:45 +0100

    feat: add mod components index file

---
+++ src/mainview/components/mods/index.ts
@@ -0,0 +1,5 @@
+export { ModSearchBar } from "./mod-search-bar";
+export { ModSortSelect } from "./mod-sort-select";
+export { ModFilterPanel } from "./mod-filter-panel";
+export { ModCard } from "./mod-card";
+export { ModList } from "./mod-list";
