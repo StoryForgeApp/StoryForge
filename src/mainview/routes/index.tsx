e9d13c6681340aa0645863f30d2b862608f30b4a
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-03-13 11:03:53 +0100

    feat: update welcome message to reflect new Story Forge branding

--- src/mainview/routes/index.tsx
+++ src/mainview/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3>Welcome to the new Story Forge - we're slowly building it out!</h3>
     </div>
   );
 }
